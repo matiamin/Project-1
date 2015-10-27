622981e1a2d136268593def4959d7936111c82e0
Matiullah Amin <matiullah243@gmail.com>
2015-10-26 20:12:20 -0400

    finished setting up and going through questions with an alert to show correct and incorrect answers

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+var quiz = [
+  {question:"who is the president of US?", answer:"obama"},
+  {question: "How many states are there in the U.S.?", answer:"50"},
+  {question: "Who is the coolest coder?", answer:"Mati"}
+];
+
+var currentQuestion = 0;
+
+$(".question-text").html(quiz[currentQuestion].question);
+
+$(".resetBoard").on ("click", function () {
+  $(".question-text").html(" ");
+});
+
+$(".submitAnswer").on ("click", function(){
+  var answer = ($(".typeAnswer").val());
+  $(".typeAnswer").text(" ");
+  event.preventDefault();
+  if (answer === quiz[currentQuestion].answer){
+    $(".result-box").text("Correct").css("color","green");
+  } else {
+    $(".result-box").text("Wrong").css("color","red");
+  }
+  currentQuestion++;
+  $(".question-text").html(quiz[currentQuestion].question);
+});
