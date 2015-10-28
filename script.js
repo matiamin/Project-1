15e800e2e5c814689dc862ccfd34cae81ebb82b9
Matiullah Amin <matiullah243@gmail.com>
2015-10-28 11:44:00 -0400

    added more trivia questions.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 var quiz = [
-  {question:"Who is the president of U.S.?", answer:["obama","Obama","barack",
-  "Barack","barack obama","Barack Obama","barack Obama","Barack obama"]},
-  {question: "How many states are there in the U.S.?", answer:["50","fifty","Fifty","FIFTY"]},
   {question: "Who is the coolest coder?", answer:["adam","Adam","Andy","andy","John","john",
-  "nick","Nick","adrian","Adrian","matt","Matt","jesse","Jesse"]}
+  "nick","Nick","adrian","Adrian","matt","Matt","jesse","Jesse"]},
+  {question:"What is Mitt Romney's middle name?", answer:["mitt","Mitt"]},
+  {question: "What's the capital of Afghanistan?", answer:["Kabul","kabul","kabol","Kabol"]},
+  {question: "Which U.S. state is nearest to the former Soviet Union? ", answer:["alaska","Alaska"]},
+  {question: "What is Canada's national animal? ", answer:["beaver","Beaver"]},
+  {question: "Who is it that removed Broccoli from the WH menus?", answer:["bush","Bush", "George", "george", "george bush", "George Bush", "George bush"]},
 ];
 
 var currentQuestion = 0;
@@ -15,31 +17,38 @@ $(".question-text").hide();
 $(".score").hide();
 $(".submitAnswer").off("click");
 
+//When player name is submited, it appears under with the first quesiton. The start button and the input box disappear.
 $(".submitName").on("click", function (){
   event.preventDefault();
   $(".displayName").text($(".playerName").val()+"'s");
   $(".playerName").val("");
   $(".question-text").show("");
   $(".question-text").html(quiz[0].question);
-  $(".submitAnswer").on ("click", function(){
+  $(".form").hide();
 
+  //When answer is submitted, qestion number increments by 1.
+  $(".submitAnswer").on ("click", function(){
     $(".score").show();
     questionCount++;
-    $(".result-box").text(+questionCount +" "+"of 5 Qs" ).css("color","green");
-    /*why isn't is displaying the message in the text body? Instead, it disappears.*/
-    if (questionCount===quiz[currentQuestion].question) {
-      event.preventDefault();
-      $(".question-text").text("Reset to play again");
+    $(".result-box").text(+questionCount +" "+"of 6 Qs" ).css("color","green");
+    if (questionCount==quiz.length) {
+      $(".result-box").text("Reset to play again");
+      $(".question-text").hide();
+      $(".displayName").hide();
+      $(".submitAnswer").off("click");
+
     }
+    //checking whether answer is correct or not, counting by 1 every time it is correct.
     var answer = ($(".typeAnswer").val());
     event.preventDefault();
     for (i=0; i<=quiz[currentQuestion].answer.length;i++) {
       if (answer === quiz[currentQuestion].answer[i]){
         answerCount++;
-        $(".score").html("correct answers:"+" "+answerCount +" "+ "out 5");
+        $(".score").html("correct answers:"+" "+answerCount +" "+ "out of 6");
       }
     }
 
+    //Moving to the next question and retrieving its text after each answer submission.
     currentQuestion++;
     $(".question-text").html(quiz[currentQuestion].question);
     $(".typeAnswer").val("");
@@ -47,14 +56,9 @@ $(".submitName").on("click", function (){
   });
 
 });
-
+  //Reset button clears up the board.
 $(".typeAnswer").val("");
 $(".resetBoard").on ("click", function () {
   $(".question-text").html(" ");
 
 });
-
-/*After name submittion, even if in the middle of a game, it should reset the entire board
-with a starting set of score and first question and show the new player's name.*/
-/*Showing a "Play again" message after a player has answered all questions.*/
-/*clicking on the startbutton without any value in the input should show a reset blank board.*/
