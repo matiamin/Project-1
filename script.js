dbca2b4de87fd87785d491a98e618a6d3dc6bdf0
Matiullah Amin <matiullah243@gmail.com>
2015-10-28 00:13:37 -0400

    added player name and scoring functionalities

--- script.js
+++ script.js
@@ -2,38 +2,55 @@
 var quiz = [
   {question:"Who is the president of U.S.?", answer:["obama","Obama","barack",
   "Barack","barack obama","Barack Obama","barack Obama","Barack obama"]},
-
   {question: "How many states are there in the U.S.?", answer:["50","fifty","Fifty","FIFTY"]},
-
   {question: "Who is the coolest coder?", answer:["adam","Adam","Andy","andy","John","john",
   "nick","Nick","adrian","Adrian","matt","Matt","jesse","Jesse"]}
 ];
 
 var currentQuestion = 0;
-// var currentAnswer = 0;
+var questions = 0;
 var count = 0;
 
-$(".question-text").html(quiz[currentQuestion].question);
-$(".resetBoard").on ("click", function () {
-  $(".question-text").html(" ");
-  // $(".question-text").html(quiz[currentQuestion].question);
-});
+$(".question-text").hide();
+$(".score").hide();
+$(".submitAnswer").off("click");
 
-$(".submitAnswer").on ("click", function(){
-  var answer = ($(".typeAnswer").val());
-  // var player = ($(".playerName").val());
+$(".submitName").on("click", function (){
   event.preventDefault();
+  $(".displayName").text($(".playerName").val()+"'s");
+  $(".playerName").val("");
+  $(".score").show();
+  $(".question-text").show();
+  $(".submitAnswer").on ("click", function(){
+    questions++;
 
-  for (i=0; i<=quiz[currentQuestion].answer.length;i++) {
-    console.log(quiz[currentQuestion].answer[i]);
-    if (answer === quiz[currentQuestion].answer[i]){
-      $(".result-box").text("Correct").css("color","green");
-      count++;
-      $(".score").html("Your Current Score:" +count);
-    } else {
-      $(".result-box").text("Wrong").css("color","red");
+    $(".result-box").text(+questions +" "+"of 5 Qs" ).css("color","green");
+    var answer = ($(".typeAnswer").val());
+    event.preventDefault();
+    for (i=0; i<=quiz[currentQuestion].answer.length;i++) {
+      if (answer === quiz[currentQuestion].answer[i]){
+        count++;
+        $(".score").html("correct answers:"+" "+count +" "+ "out 5");
+      }
     }
     currentQuestion++;
     $(".question-text").html(quiz[currentQuestion].question);
-  }
+    $(".typeAnswer").val("");
+
+  });
+
 });
+
+$(".typeAnswer").val("");
+$(".question-text").html(quiz[currentQuestion].question);
+$(".resetBoard").on ("click", function () {
+  $(".question-text").html(" ");
+});
+
+
+//After name submittion, whether in the middle of the game, it should reset the entire board.
+//The trick is to keep the name of the new player.
+//How do I hide the answer input and the auto response of Correct and Wrong after a few secs of
+//submitting the answer?
+//Set up a function to display winner or loser based on how many questions are answered correctly
+//at the end. And by clicking the reset button or putting in name again should restart the game.
